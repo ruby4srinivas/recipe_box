8385a8a253cea8dd7888e4b09c957203bf82da18
srinivas <srinivas4ruby@gmail.com>
2016-02-05 21:44:51 +0530

    cosmetic changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,6 +9,6 @@ class Recipe < ActiveRecord::Base
                                 reject_if: proc { |attributes| attributes['step'].blank? },
                                 allow_destroy: true
   validates :title, :description, :image , presence: true
-  has_attached_file :image, styles: { :medium => "300x300#" }
+  has_attached_file :image, styles: { :medium => "250x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
